cad2a30ba50765711ac435d5c64082a559c5a09a
emmaodia <odia.emma@gmail.com>
2020-11-06 02:04:25 +0100

    UPDATE: Loaded Eva UI Kitten Library in App.js to make available to entire project.

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider } from '@ui-kitten/components';
 import AppNavigation from './navigation/Index';
 
 export default function App() {
   return (
-   <AppNavigation />
+   <ApplicationProvider {...eva} theme={eva.dark}>
+    <AppNavigation />
+   </ApplicationProvider>
   );
 }
