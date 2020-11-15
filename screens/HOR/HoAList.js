7b727963c72160996b6b465d9ec2c14a18995ba6
emmaodia <odia.emma@gmail.com>
2020-11-15 23:25:12 +0100

    SCREEN: House of Assembly Members List.

---
+++ screens/HOR/HoAList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+const HoAList = props => {
+  return (
+    <View style={styles.container}>
+        <TouchableOpacity onPress={() => props.navigation.navigate('Dashboard')}>
+            <Text>List of House of Assembly Members Screen!</Text>
+        </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default HoAList;
